85ae49e8783473dbb456cc04d8e1a6aa887f8087
InhollandComposites <55623812+InhollandComposites@users.noreply.github.com>
2020-12-14 11:33:34 +0100

    Update1

--- Assets/SizeCalc.cs
+++ Assets/SizeCalc.cs
@@ -5,28 +5,39 @@ using UnityEngine.UI;
 
 public class SizeCalc : MonoBehaviour
 {
-    public float Thickness = 0;
-    public float Tangens = 0;
-    public float Fraction = 0;
-    public float maxPlyDiameter = 0;
+
+    public float nPlies;
+    public float[] Lpatch;
+    public float[] plyDiameter;
+    public float patchAngle = 3f;
+    public float Dhole;
+    public GameObject ply;
     public DataPool dataPool;
+    public float materialThickness;
 
-    public void ThicknessEstimation()
-    {
-         dataPool = GameObject.Find("ManagerObject").GetComponent<DataPool>();
-    }
-
+    
     public void Update()
     {
-        Thickness = 2 * dataPool.nDepth;
-        dataPool.Thickness = Thickness;
-        Debug.Log(Thickness);
-
-        dataPool.Fraction = (Thickness / dataPool.Tangens);
-
-        dataPool.patchsize = (dataPool.Fraction + (10*dataPool.maxPlyDiameter));
+        dataPool = GameObject.Find("ManagerObject").GetComponent<DataPool>();
+        Dhole = dataPool.maxPlyDiameter;
+        materialThickness = dataPool.nDepth;
     }
 
+    public void CalcPlies(int n, float materialThickness, float maxPlyDiameter)
+    {
+        Lpatch = new float[n];
+        plyDiameter = new float[n];
 
+        for (int i = 0; i<n; i++)
+        {
+            float k = materialThickness / n * (n - i);
+            Lpatch[i] = 2 * k / Mathf.Tan(patchAngle) + Dhole;
+            plyDiameter[i] = plyDiameter[i + 1] - 0.5f;
+        }
+    }
+
+    public void ApplyPlyParameters(int index)
+    {
+        ply.transform.localScale = new Vector3(plyDiameter[index], Lpatch[index], plyDiameter[index]);
+    }
 }
-   
